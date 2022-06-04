ac5943a653d9a96ec15a54ed8f10c416246b619b
Bobby Juncosa <bobbyjuncosa@gmail.com>
2022-06-04 08:33:15 -0400

    using env var

--- examples/lead_info.php
+++ examples/lead_info.php
@@ -3,9 +3,10 @@ require '../init.php';
 
 try {
   $access_token = getenv('EDGEWISE_ACCESS_TOKEN');
+  $lead_id = getenv('EDGEWISE_LEAD_ID');
+
   $config = ["access_token" => $access_token];
   $edgewise = new \Edgewise\EdgewiseClient($config);
-  $lead_id = 5000;
 
   print_r($edgewise->leads->get($lead_id));
 
